3a0b1315a98f2e4f272089669e86550d0f0754b5
giovannidefilippi <gdefilipp393@gmail.com>
2017-11-06 11:02:53 +0100

    commit

--- CentroSportivo/src/it/unisalento/pps/view/AreaIscrizioneDisciplina.java
+++ CentroSportivo/src/it/unisalento/pps/view/AreaIscrizioneDisciplina.java
@@ -77,52 +77,9 @@ public class AreaIscrizioneDisciplina extends JFrame{
 		scelta.setText("Iscrizione ai  CORSI  ed agli EVENTI di "+disciplina.getNome().toUpperCase());
 		scelta.setFont(new Font("sansserif",Font.BOLD,30));
 		titolo.add(scelta);
-		centroPnl.add(titolo);
+		centroPnl.add(titolo);		
 		
-		//corsi = EventoBusiness.getInstance().getCorsiByIdDisciplina(disciplina.getIdDisciplina());
-		livelli = LivelloBusiness.getInstance().getLivelli();
-		
-		/*Spazio spazio;
-		TipoEvento tipoevento;
-		
-		
-		if(corsi.size()>0) {
-			for(int i=0;i<corsi.size();i++) {
-				System.out.println(corsi.size());
-				
-				disciplina = DisciplinaBusiness.getInstance().getDisciplinaById(corsi.get(i).getDisciplina());
-				spazio = SpazioBusiness.getInstance().getSpazioById(corsi.get(i).getSpazio());
-				tipoevento = TipoEventoBusiness.getInstance().getTipoEventoById(corsi.get(i).getTipo());
-			
-				String giorno_inizio= corsi.get(i).getDataInizio().toString().substring(8,10);
-				String mese_inizio = corsi.get(i).getDataInizio().toString().substring(5,7);
-				String anno_inizio =corsi.get(i).getDataInizio().toString().substring(0,4);	
-			
-				String giorno_fine= corsi.get(i).getDataFine().toString().substring(8,10);
-				String mese_fine = corsi.get(i).getDataFine().toString().substring(5,7);
-				String anno_fine =corsi.get(i).getDataFine().toString().substring(0,4);
-			
-				campo_evento = new JRadioButton(corsi.get(i).getIdEvento()+"  "+disciplina.getNome()+ ": INIZIA IL:  " +giorno_inizio+ "/"+mese_inizio+ "/" +anno_inizio+"   ORARI:  " +corsi.get(i).getTurno()+ "   TERMINA IL:  "+giorno_fine+ "/"+mese_fine+ "/" +anno_fine+"  LUOGO:  "+spazio.getNome() +"  TIPO:  "+tipoevento.getTipo()+ "  COSTO:  € "+disciplina.getCostoMensile()+"0");	
-				campo_evento.setFont(new Font("sansserif",Font.BOLD,20));
-				
-				JPanel contenuto1=new JPanel(new GridLayout(2,1));
-				contenuto1.add(contenutoVuoto);
-				contenuto1.add(campo_evento);
-				contenuto.setLayout(new GridLayout(corsi.size(),1));
-				contenuto.add(contenuto1);
-				
-				group_evento.add(campo_evento);
-			}
-		}
-		else {
-			JLabel nessunaoccorrenza = new JLabel("Nessun Corso nel sistema");	
-			nessunaoccorrenza.setFont(new Font("sansserif",Font.BOLD,20));
-			contenutoVuoto.add(nessunaoccorrenza);
-			centroPnl.add(contenutoVuoto);		
-		}			
-		
-		centroPnl.add(contenuto);*/
-
+		livelli = LivelloBusiness.getInstance().getLivelli();		
 		
 		selPagamento.setFont(new Font("sansserif",Font.BOLD,22));
 		contenutopagamento.add(selPagamento);
@@ -159,7 +116,7 @@ public class AreaIscrizioneDisciplina extends JFrame{
 		indietro.addActionListener(ascoltatoreBackHome);
 		indietro.setActionCommand(AscoltatoreBackHome.D1);
 		contenutotasti.add(indietro);
-		iscrizione.addActionListener(new AscoltatoreConfermaIscrizioni(this, tesserato));
+		iscrizione.addActionListener(new AscoltatoreConfermaIscrizioni(this, tesserato,disciplina));
 		contenutotasti.add(iscrizione);
 		centroPnl.add(contenutolivello);
 		sudPnl.add(contenutotasti);
